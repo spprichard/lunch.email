c37b488b995444a17692d4d700a98f82067b364d
stevenprichard <steven.prichard@boldcommerce.com>
2017-08-23 18:20:42 -0500

    init commit

---
+++ Sources/main.swift
@@ -0,0 +1,24 @@
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+
+// Set up Routes
+let routes = Routes([
+    Route(method: .get, uri: "/", handler: { (request, response) in
+        response.setBody(string: "Hello, World!")
+        response.completed()
+    })
+])
+
+// Config Server
+let server = HTTPServer()
+server.serverPort = 8181
+server.addRoutes(routes)
+
+// Run Server
+do {
+    try server.start()
+}catch PerfectError.networkError(let error, let message) {
+    print("Error: \(error) -> Message: \(message)")
+}
